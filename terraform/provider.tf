3cbe79b1348845f7d7f6bc81ce9a1776a192e5d9
Thomas Nesset <tim98@live.no>
2025-04-18 22:22:15 +0200

    typo

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
   features {}
 
-  client_id       = var.azure.client_id
+  client_id       = var.azure_client_id
   tenant_id       = var.azure_tenant_id
   subscription_id = var.azure_subscription_id
 }
