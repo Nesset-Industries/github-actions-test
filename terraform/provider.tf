4da4abe0f9bedab54143971ba910b64675141753
Thomas Nesset <tim98@live.no>
2025-04-18 22:33:23 +0200

    changes

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,6 +4,7 @@ provider "azurerm" {
   client_id       = var.azure_client_id
   tenant_id       = var.azure_tenant_id
   subscription_id = var.azure_subscription_id
-  use_msi         = false
+
   use_cli         = false
+  use_msi         = false
 }
