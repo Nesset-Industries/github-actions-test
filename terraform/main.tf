984bad0aa35f88c4007579ccdf445128718a77b8
Thomas Nesset <tim98@live.no>
2025-04-18 21:56:41 +0200

    Configured tf-plan job

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  cloud {
+    organization = "Nesset Industries"
+    
+    workspaces {
+      name = "github-actions-test"
+    }
+  }
+}
+
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
