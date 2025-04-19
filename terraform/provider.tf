19de9e1889c32d01d8f9526380d160b47af622aa
Thomas Nesset <tim98@live.no>
2025-04-19 16:52:41 +0200

    changes

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,10 +1,5 @@
 provider "azurerm" {
   features {}
-
-  client_id       = var.azure_client_id
-  tenant_id       = var.azure_tenant_id
-  subscription_id = var.azure_subscription_id
-
   use_oidc        = true
   use_cli         = false
   use_msi         = false
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,18 +1,3 @@
-variable "azure_client_id" {
-  description = "Azure Client ID"
-  type        = string
-}
-
-variable "azure_tenant_id" {
-  description = "Azure Tenant ID"
-  type        = string
-}
-
-variable "azure_subscription_id" {
-  description = "Azure Subscription ID"
-  type        = string
-}
-
 variable "resource_group_name" {
   description = "Name of the Resource Group"
   type        = string
