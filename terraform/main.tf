46889b7b01aebb0cda6d718213e863c0c8b30a4c
Thomas Nesset <tim98@live.no>
2025-04-20 14:32:58 +0200

    revert

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,14 +8,9 @@ terraform {
 
   # Configure the azurerm backend
   backend "azurerm" {
-    resource_group_name  = "rg-nesset-github-actions-test-state"      # The name of the resource group where the storage account exists
-    storage_account_name = "sagithubactionsteststate"      # The name of your storage account
-    container_name       = "sc-state"    # The name of the container within the storage account
-    key                  = "terraform.tfstate" # The path/filename for your state file within the container
+    resource_group_name  = "rg-nesset-github-actions-test-state"      
   }
 }
-
-# Your resource blocks (e.g., resource "azurerm_resource_group" "example" { ... }) go here
 
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
