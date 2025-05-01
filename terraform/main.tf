6595d64f1fbdf95ad988a881f1b70b36187126bd
Thomas Nesset <tim98@live.no>
2025-05-01 14:08:46 +0200

    static website init

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,60 @@ terraform {
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = var.resource_group_name
+  name     = var.example_resource_group_name
   location = var.location
+}
+
+resource "azurerm_resource_group" "static_website" {
+  name     = var.static_website_resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "static_website" {
+  name                     = var.static_website_storage_account_name
+  resource_group_name      = azurerm_resource_group.static_website.name
+  location                 = azurerm_resource_group.static_website.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+
+  static_website {
+    index_document = "index.html"
+    error_404_document = "404.html"
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_cdn_profile" "static_website" {
+  name                = var.cdn_profile_name
+  resource_group_name = azurerm_resource_group.static_website.name
+  location            = azurerm_resource_group.static_website.location
+  sku                 = "Standard_Microsoft"
+
+  tags = var.tags
+}
+
+resource "azurerm_cdn_endpoint" "static_website" {
+  name                = var.cdn_endpoint_name
+  profile_name        = azurerm_cdn_profile.static_website.name
+  location            = azurerm_resource_group.static_website.location
+  resource_group_name = azurerm_resource_group.static_website.name
+
+  origin {
+    name      = "staticwebsite"
+    host_name = azurerm_storage_account.static_website.primary_web_host
+  }
+
+  tags = var.tags
+}
+
+# Output the website URL
+output "static_website_url" {
+  value = azurerm_storage_account.static_website.primary_web_endpoint
+}
+
+# Output the CDN URL
+output "cdn_endpoint_url" {
+  value = "https://${azurerm_cdn_endpoint.static_website.host_name}"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,45 @@
-variable "resource_group_name" {
+variable "example_resource_group_name" {
   description = "Name of the Resource Group"
   type        = string
   default     = "example-rg"
+}
+
+variable "static_website_resource_group_name" {
+  description = "Name of the Static Website Resource Group"
+  type        = string
+  default     = "static-website-rg"
+}
+
+variable "static_website_storage_account_name" {
+  description = "Name of the Static Website Storage Account"
+  type        = string
+  default     = "tnesastaticwebsite"
+}
+
+variable "cdn_profile_name" {
+  description = "Name of the CDN Profile"
+  type        = string
+  default     = "tnesacdnprofile"
+}
+
+variable "cdn_endpoint_name" {
+  description = "Name of the CDN Endpoint"
+  type        = string
+  default     = "tnesacdnendpoint"
 }
 
 variable "location" {
   description = "Azure Region for Resource Group"
   type        = string
   default     = "West Europe"
+}
+
+variable "tags" {
+  description = "Tags for the resources"
+  type        = map(string)
+  default     = {
+    environment = "prod"
+    project     = "github-actions"
+    managed_by  = "terraform"
+  }
 }
